cc4b9464fb66f0a6c0667c63c1922328131d0d87
kelicom <735579768@qq.com>
2019-12-13 13:31:23 +0800

    f

--- scripts/Config.py
+++ scripts/Config.py
@@ -8,7 +8,7 @@ accessKeySecret = 'aOLFLanFp0YQDKZeJN4VAuXA1sKGKB'
 endpoint = 'oss-cn-beijing.aliyuncs.com'
 bucketName = 'xiangce-archive'
 
-logPath = os.getcwd() + '/../logs/baklog'
+logPath = os.getcwd() + '/../logs/bak'
 # 数据库sql备份,请确保mysqldump在环境变量中能执行,也可以直接在下面设置
 mysqldump_path = ''
 db_host = "127.0.0.1"
