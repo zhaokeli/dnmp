1ecc1e65a0b2c158d93b62b6297852d6869a9035
kelicom <735579768@qq.com>
2020-10-26 21:55:11 +0800

    添加composer2

---
+++ services/composer/push.sh
@@ -0,0 +1,17 @@
+# 镜像名字
+imageName=mokuyu/composer
+
+# 构建
+docker build . -t ${imageName}
+
+# 登陆
+docker login --username=735579768@qq.com registry.cn-beijing.aliyuncs.com
+
+# push
+imageId=`docker images -q ${imageName}`
+docker tag ${imageId} registry.cn-beijing.aliyuncs.com/${imageName}:latest
+docker push registry.cn-beijing.aliyuncs.com/${imageName}:latest
+
+#拉取并重命名镜像
+docker pull registry.cn-beijing.aliyuncs.com/mokuyu/composer2
+docker tag `docker images -q registry.cn-beijing.aliyuncs.com/mokuyu/composer2` mokuyu/composer2
